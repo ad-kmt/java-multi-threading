eb40bdd4c8bb7422c515068b721d28678233bcea
KMT <kumawat.aditya743@gmail.com>
2023-02-04 19:51:05 +0530

    Deadlock problem

---
+++ src/main/java/org/kmt/multithreading/resource_sharing/data_race/DataRaceExample.java
@@ -0,0 +1,40 @@
+package org.kmt.multithreading.resource_sharing.data_race;
+
+public class DataRaceExample {
+
+    public static void main(String[] args) {
+        SharedClass shared = new SharedClass();
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                shared.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                shared.checkForDataRace();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+
+    public static class SharedClass{
+        private int x = 0;
+        private int y = 0;
+
+        public void increment(){
+            x++;
+            y++;
+        }
+
+        public void checkForDataRace(){
+            if(y>x){
+                System.out.println("Y > X - Data race detected");
+            }
+        }
+    }
+
+}
